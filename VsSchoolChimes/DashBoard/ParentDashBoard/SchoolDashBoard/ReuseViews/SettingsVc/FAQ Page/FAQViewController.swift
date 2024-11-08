d3697ec1aa26f2fe463705a9509da8279cbae045
murugansavyasasy <murugan@savyasasy.com>
2024-11-08 13:08:54 +0530

    saranAdd

--- VsSchoolChimes/DashBoard/ParentDashBoard/SchoolDashBoard/ReuseViews/SettingsVc/FAQ Page/FAQViewController.swift
+++ VsSchoolChimes/DashBoard/ParentDashBoard/SchoolDashBoard/ReuseViews/SettingsVc/FAQ Page/FAQViewController.swift
@@ -13,7 +13,7 @@ class FAQViewController: UIViewController {
     @IBOutlet weak var submitbutton: UIButton!
     @IBOutlet weak var tableview: UITableView!
     
-    let identifier = "FAQTableViewCell"
+  
     var expandedIndexPaths: Set<IndexPath> = []
     var index : Int? = nil
     override func viewDidLoad() {
