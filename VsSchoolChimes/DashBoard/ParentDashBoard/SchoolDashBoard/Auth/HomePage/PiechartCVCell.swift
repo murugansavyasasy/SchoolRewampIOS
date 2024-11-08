a15d63ecc548dc4af6a718a00368219e1529b074
murugansavyasasy <murugan@savyasasy.com>
2024-11-08 12:42:38 +0530

    newly added

---
+++ VsSchoolChimes/DashBoard/ParentDashBoard/SchoolDashBoard/Auth/HomePage/PiechartCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  PiechartCVCell.swift
+//  VsSchoolChimes
+//
+//  Created by Admin on 08/11/24.
+//
+
+import UIKit
+
+class PiechartCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
