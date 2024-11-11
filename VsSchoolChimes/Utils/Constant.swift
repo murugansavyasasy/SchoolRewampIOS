130771f7d3c39508634b2e9d82779398d94f4b3a
murugansavyasasy <murugan@savyasasy.com>
2024-11-11 10:49:31 +0530

    Language Bug Fixed SARAN

--- VsSchoolChimes/Utils/Constant.swift
+++ VsSchoolChimes/Utils/Constant.swift
@@ -14,7 +14,7 @@ import Foundation
 struct DefaultsKeys {
     static let countryId = "countryId"
     static let LoginId = "LoginId"
-    static let Language = "Language"
+    static let Language = "en"
    
     
 }
