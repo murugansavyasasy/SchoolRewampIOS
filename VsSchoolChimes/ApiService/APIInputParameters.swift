dda551858e21a4f9d96ead6b5808f1a35f92cda6
murugansavyasasy <murugan@savyasasy.com>
2025-03-15 10:49:11 +0530

    BaseUrl chnged

--- VsSchoolChimes/ApiService/APIInputParameters.swift
+++ VsSchoolChimes/ApiService/APIInputParameters.swift
@@ -6,118 +6,10 @@
 //
 
 import Foundation
-struct VersionCeck {
-    static let versionCode = "version_code"
-    static let device_type = "device_type"
-}
-struct MobileVerification {
-    static let mobileNo = "mobile_number"
-}
-struct InitiateOtp  {
-    static let mobileNo = "mobile_number"
-}
-struct ValidateUser   {
-    static let mobileNo = "mobile_number"
-    static let token = "token"
-    static let device_id = "device_id"
-    static let otp = "otp"
-}
-struct CreateMeetingList {
-    static let meeting_mode = "meeting_mode"
-    static let meeting_link = "meeting_link"
-    static let to_time = "to_time"
-    static let from_time = "from_time"
-    static let event_content = "meeting_content"
-    static let mobile_number = "mobile_number"
-    static let event_date = "meeting_date"
-    static let venue = "venue"
-    static let groups_id = "groups_id"
-    static let title = "title"
-    static let created_by = "created_by"
-    static let college_id = "college_id"
-    static let locationDetails = "locationDetails"
-    static let location_name = "location_name"
-    static let latitude = "latitude"
-    static let longitude = "longitude"
-    static let distance = "distance"
-    static let location_id = "location_id"
-}
 
-struct UpdateMeetingList {
-    static let meeting_mode = "meeting_mode"
-    static let meeting_link = "meeting_link"
-    static let to_time = "to_time"
-    static let from_time = "from_time"
-    static let event_content = "meeting_content"
-    static let mobile_number = "mobile_number"
-    static let event_date = "meeting_date"
-    static let venue = "venue"
-    static let participants = "participants"
-    static let title = "title"
-    static let id = "id"
-    static let longitude = "longitude"
-    static let latitude = "latitude"
-    static let address = "address"
-    static let distance = "distance"
-    
-    
-}
-struct Getmeetingdetails{
-    static let event_status = "event_status"
-}
-struct DeleteMeetingDetails{
-    static let id = "id"
-}
-struct get_meeting_attendance{
-    static let meeting_id = "id"
-}
-struct get_meeting_location{
-    static let meeting_id = "meeting_id"
-}
 
-struct UpdateUserdetails{
-    static let member_id = "member_id"
-    static let name = "name"
-    static let address = "address"
-    static let type_id = "type_id"
-    static let profile_url = "profile_url"
-    static let designation = "designation"
-    static let email = "email"
-    static let whatsapp_number = "whatsapp_number"
-    static let mobile_number = "mobile_number"
-    static let city = "city"
-    static let pincode = "pincode"
-    static let state = "state"
-    static let dob = "dob"
-    static let gender = "gender"
-    static let college_id = "college_id"
-    static let college_name = "college_name"
-    static let active_bank_id = "active_bank_id"
-    static let pan_number = "pan_number"
-    static let groups_id = "groups_id"
-    static let account_details = "account_details"
-    
-}
 
-struct AccountDetails{
-    static let bank_name = "bank_name"
-    static let branch = "branch"
-    static let account_holder_name = "account_holder_name"
-    static let account_type = "account_type"
-    static let ifsc_code = "ifsc_code"
-    static let active_bank_id = "active_bank_id"
-}
-struct AddLocation{
-    static let longitude = "longitude"
-    static let latitude = "latitude"
-    static let address = "address"
-    static let distance = "distance"
-    static let location_name  = "location_name"
-    static let landkmark = "landkmark"
-    static let college_id  = "college_id"
-    static let meeting_id  = "meeting_id"
-}
-
+class 
 
 class ApitTypeSringFile{
     static var POST = "POST"
@@ -125,17 +17,5 @@ class ApitTypeSringFile{
     static var Delete = "DELETE"
     static var Put = "PUT"
 }
-struct Get_location_historyStr{
-    
-    static let college_id = "college_id"
-    
-}
+ 
 
-struct Punch_inStr{
-    
-    static let meeting_id = "meeting_id"
-    static let member_id = "member_id"
-    
-    
-    
-}
--- VsSchoolChimes/ApiService/AppConstant.swift
+++ VsSchoolChimes/ApiService/AppConstant.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 enum Environments : String{
-    case development = "http://rgu.savyasasy.com/v1/setup/"
+    case development = "http://apiv7.schoolchimes.net/app/"
 }
 
 struct AppConficuration{
@@ -16,35 +16,10 @@ struct AppConficuration{
 struct ServiceUrl{
     static let baseurl = AppConficuration.enviranment.rawValue
     static var token = ""
-    static var awsBucketName = "rgu-meet-files"
+    static var awsBucketName = ""
     
-    
-    static let country_list             = "setup/countries"
     static let version_check            = "/setup/version-check"
-    static let validate_mobile_number   = "validate-mobile-number"
-    static let initiate_otp             = "initiate-otp"
-    static let validate_user            = "validate-user"
-    static let auto_login               = "auto-login"
-    static let get_members              = "get-members"
-    static let  get_groups              = "get-groups"
-    
-    static let create_meeting           = "create-meeting"
-    static let get_meeting_details      = "get-meeting-details-history"
-    static let delete_meeting_details   = "delete-meeting/"
-    static let update_meeting_details   = "update-meeting-details"
-    static let get_meeting_details_list = "get-meeting-details-list"
-    static let get_meeting_attendance   = "get-meeting-attendance"
-    static let add_location             = "add-location"
-    static let Get_Meeting_Location     = "get-meeting-location"
-    static let get_location_history     = "get-location-history"
-    static let attendance_members_Punch = "create-attendance" // Punch
-    static let update_member_details    = "update-member-details"
-    static let get_claim_details        = "get-claim-list"
-    static let create_claim             = "create-claim"
-    static let getclaimgroup            = "get-claim-for-group"
-    static let claim_validation            = "claim-validation"
-  
-   
+    static let country_list             = "setup/countries"
 }
 func isValidEmail(_ email: String) -> Bool {
     let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$"
