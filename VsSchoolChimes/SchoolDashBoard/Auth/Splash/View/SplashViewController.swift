9b40a0ad4453e1f55fd55c7e59142ee216fc3f8f
murugansavyasasy <murugan@savyasasy.com>
2025-03-17 10:34:58 +0530

    api

--- VsSchoolChimes/SchoolDashBoard/Auth/Splash/View/SplashViewController.swift
+++ VsSchoolChimes/SchoolDashBoard/Auth/Splash/View/SplashViewController.swift
@@ -33,49 +33,131 @@ class SplashViewController: UIViewController {
         loginId = defaults.string(forKey:DefaultsKeys.LoginId)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [self] in
-          
-            if countryId == nil{
+            Version_Check()
+        }
+        
+    }
+ 
+    func Version_Check() {
+        APIService.shared
+            .makeApi(url: ServiceUrl.version_check, parameters: [
+                COMMON_PARAMETER.device_type : API_CALL_HOTCODE.device_type,
+                COMMON_PARAMETER.version_code: API_CALL_HOTCODE.Version_Code
+            ], type: ApitTypeSringFile.POST, token: ServiceUrl.token) { [self] (
+                result: Result<VersionCheckResponse,
+                Error>
+            ) in
+                switch result {
+                case .success(let successMessage):
+                    if successMessage.status == true {
+                        DispatchQueue.main.async { [self] in
+                            if successMessage.data?.first?.forceUpdate == true && successMessage.data?.first?.updateAvailable == true {
+                                
+                                let updateAlert = UIAlertController(
+                                    title: "Needs to Update",
+                                    message: "New updates are available. Would you like to update them now?",
+                                    preferredStyle: .alert
+                                )
+                                updateAlert.addAction(UIAlertAction(title: "Update", style: .default, handler: { _ in
+                                    self.callAppStore(AppStoreLink: successMessage.data?.first?.redirect_url ?? "")
+                                }))
+                                
+                                present(updateAlert, animated: true, completion: nil)
+                                
+                                
+                            }else if successMessage.data?.first?.forceUpdate == false && successMessage.data?.first?.updateAvailable == true{
+                                
+                                let forceUpdateAlert = UIAlertController(
+                                    title: "Needs to Update",
+                                    message: "New updates are available. Would you like to update them now?",
+                                    preferredStyle: .alert
+                                )
+                                forceUpdateAlert.addAction(UIAlertAction(title: "Not Now", style: .default, handler: { _ in
+                                    //                                    self.VerifyLogin()
+                                    self.AppFlowChecking()
+                                }))
+                                forceUpdateAlert.addAction(UIAlertAction(title: "Update", style: .default, handler: { _ in
+                                    self.callAppStore(AppStoreLink: successMessage.data?.first?.redirect_url ?? "")
+                                }))
+                                present(forceUpdateAlert, animated: true, completion: nil)
+                            }else{
+                                
+                                self.AppFlowChecking()
+                                
+                            }
+                            
+                        }
+                    }else{
+                        DispatchQueue.main.async {
+                            
+                            let updateAlert = UIAlertController(
+                                title: "",
+                                message: successMessage.message,
+                                preferredStyle: .alert
+                            )
+                            updateAlert.addAction(UIAlertAction(title: "ok", style: .default, handler: { _ in
+                                self.dismiss(animated: true)
+                            }))
+                            
+                            self.present(
+                                updateAlert,
+                                animated: true,
+                                completion: nil
+                            )
+                            
+                        }
+                    }
+                    
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        
+    }
+ 
+    func AppFlowChecking(){
+        
+        if countryId == nil{
+            
+            let vc = CountryVc(nibName: nil, bundle: nil)
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+            
+        }else{
+            
+            if loginId == nil{
                 
-                let vc = CountryVc(nibName: nil, bundle: nil)
+                let vc = LoginVc(nibName: nil, bundle: nil)
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true)
-                
             }else{
                 
-                if loginId == nil{
-                    
-                    let vc = LoginVc(nibName: nil, bundle: nil)
-                    vc.modalPresentationStyle = .fullScreen
-                    self.present(vc, animated: true)
-                }else{
-                    
-                    let vc = PriorityViewController1(nibName: nil, bundle: nil)
-                    vc.modalPresentationStyle = .fullScreen
-                    self.present(vc, animated: true)
-                }
-             
-                
+                let vc = PriorityViewController1(nibName: nil, bundle: nil)
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true)
             }
+            
+            
         }
-        
     }
     
-    
-    
-    func loginVerify(){
-      
-        loginAPI.postMethod(CountryID: 1, DeviceType: "iphone", MobileNumber: "9003769500", Password: "1234", SecureID: "2126A741-1042-434D-A331-61CA7A66378A")  { [self] result in
-            switch result {
-               case .success(let response):
-                   print("Successfully got response:", response)
-                
-                logindata = response
-                   // You can access the globalLoginResponseData variable anywhere now
-               case .failure(let error):
-                   print("Error:", error.localizedDescription)
-               }
-        }
+    func callAppStore (AppStoreLink : String)
+    {
         
+        let myUrl = AppStoreLink
+        if let url = URL(string: "\(myUrl)"), !url.absoluteString.isEmpty {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        guard let url = URL(string: "\(myUrl)"), !url.absoluteString.isEmpty else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-  
 }
+    
+
+
+  
+
