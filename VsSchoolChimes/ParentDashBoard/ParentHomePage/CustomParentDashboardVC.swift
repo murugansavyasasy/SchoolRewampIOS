c5c510c423a899baaa21d083388099d63b6f49be
Murugan N <murugan@savyasasy.com>
2025-09-22 10:57:23 +0530

    commit for latest

--- VsSchoolChimes/ParentDashBoard/ParentHomePage/CustomParentDashboardVC.swift
+++ VsSchoolChimes/ParentDashBoard/ParentHomePage/CustomParentDashboardVC.swift
@@ -257,7 +257,7 @@ class CustomParentDashboardVC: UIViewController, UICollectionViewDelegate, UICol
        
         let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(hideSideMenu))
         swipeGesture.direction = .left
-        menuVC.view.addGestureRecognizer(swipeGesture)
+        dimView.addGestureRecognizer(swipeGesture)
         
         menuVC.delegate = self
         window.addSubview(menuVC.view)
