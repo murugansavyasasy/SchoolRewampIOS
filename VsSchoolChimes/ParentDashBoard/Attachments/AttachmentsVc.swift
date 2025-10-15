3e2fe8cc25d810ef9b7b7aefd54286fb78c172ae
Murugan N <murugan@savyasasy.com>
2025-10-15 09:57:14 +0530

    bugs fixed by Lakshmanan

--- VsSchoolChimes/ParentDashBoard/Attachments/AttachmentsVc.swift
+++ VsSchoolChimes/ParentDashBoard/Attachments/AttachmentsVc.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class AttachmentsVc: UIViewController {
     
-    @IBOutlet weak var noRecordStack: UIStackView!
     @IBOutlet weak var searchBtn: UIButton!
     @IBOutlet weak var noDataLabel: UILabel!
     @IBOutlet weak var backBtn: UIButton!
@@ -18,30 +17,39 @@ class AttachmentsVc: UIViewController {
     @IBOutlet weak var tv: UITableView!
     @IBOutlet weak var MenuNameLbl: UILabel!
     @IBOutlet weak var TitleLbl: UILabel!
+    @IBOutlet weak var noDataImage: UIImageView!
     
     var attachmentFiles: [[FilePath]]?
     var studentDetails = UserDefaultFileManager.get_child_Details()
     var attachmentData = [Attachment]()
     var filteredAttachments:[Attachment]?
-    var SearchAttachments:[Attachment]?
     var isHeaderExpandedDict: [Int: Bool] = [:]
     var search = true
     var isExpanded: Bool = false
     var clickedMessageId: String?
+    var shouldShowFooter = true
+    var shouldShowFooterLabel = false
+    var ArchiveMessage = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         TitleLbl.configureAsBackTitle(firstLine: studentDetails?.name ?? "", secondLine: "\(studentDetails?.standard_name ?? "") - \(studentDetails?.section_name ?? "")")
         MenuNameLbl.text = MenuStringFile.selectedMenuName
+        searchBar.isHidden = true
         searchBar.searchTextField.addDoneButton()
         searchBar.delegate = self
         searchBar.placeholder = CommonStringFile.Search
         searchBar.backgroundImage = UIImage()
+        noDataImage.isHidden = true
+        noDataLabel.isHidden = true
         tv.register(
             UINib(nibName: CellConfingName.AttachTvHeader, bundle: nil),
             forHeaderFooterViewReuseIdentifier: CellConfingName.AttachTvHeader
         )
         tv.register(UINib(nibName: CellConfingName.ContentCell, bundle: nil), forCellReuseIdentifier: CellConfingName.ContentCell)
+        
+        tv.delegate = self
+        tv.dataSource = self
         
         fetchAttachments()
     }
@@ -61,7 +69,7 @@ class AttachmentsVc: UIViewController {
             searchBar.becomeFirstResponder()
         }else{
             searchBar.searchTextField.text = ""
-//            filteredAttachments = attachmentHeaders
+            //            filteredAttachments = attachmentHeaders
             tv.reloadData()
             view.endEditing(true)
         }
@@ -87,40 +95,98 @@ class AttachmentsVc: UIViewController {
                 
                 switch result {
                 case .success(let response):
-                    if response.status == true {
-                        //                        self.hideView(ishide: true)
-                        
-                        self.noRecordStack.isHidden = true
-                        self.noDataLabel.isHidden = true
-                        self.tv.isHidden = false
-                        self.searchBtn.isHidden = false
-                        self.attachmentData = response.data ?? []
-                        self.filteredAttachments = response.data
-                        self.SearchAttachments = response.data
-                        
                     
+                    self.attachmentData = response.data ?? []
+                    self.filteredAttachments = response.data
+                    let isEmpty = self.filteredAttachments?.isEmpty ?? false
+                    self.searchBtn.isHidden = isEmpty
+                    self.noDataImage.isHidden = !isEmpty
+                    self.noDataLabel.isHidden = !isEmpty
+                    self.noDataLabel.text = response.message
+                    
+                    if !isEmpty{
                         if self.clickedMessageId != ""{
                             self.loadDataAndScrollIfNeeded()
                         }
-                        self.tv.delegate = self
-                        self.tv.dataSource = self
-                        self.tv.reloadData()
-                    } else {
-                        
-                        self.noRecordStack.isHidden = false
-                        self.noDataLabel.isHidden = false
-                        self.noDataLabel.text = response.message ?? ""
-                        self.tv.isHidden = true
-                        self.searchBtn.isHidden = true
                     }
                     
-                case .failure(_):
-                    ""
+                    self.tv.reloadData()
+                    
+                    
+                case .failure(let error):
+                    self.searchBtn.isHidden = true
+                    self.noDataImage.isHidden = false
+                    self.noDataLabel.isHidden = false
+                    self.noDataLabel.text = error.localizedDescription
                 }
             }
         }
     }
     
+    private func fetchAttachments_Archive() {
+        if #available(iOS 15.0, *) {
+            showActivityLoader()
+        }
+        
+        APIService.shared.makeApi(
+            url: ServiceUrl.comm_communication_attachment_list_archive,
+            parameters: [:],
+            type: ApitTypeSringFile.GET,
+            token:studentDetails?.access_token ?? ""
+        ) { [weak self] (result: Result<AttachmentsResponse, Error>) in
+            
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                if #available(iOS 15.0, *) {
+                    self.hideActivityLoader()
+                }
+                
+                switch result {
+                case .success(let response):
+                    if response.status == true {
+                        
+                        self.attachmentData.append(contentsOf: response.data ?? [])
+                        self.filteredAttachments = self.attachmentData
+                        let isEmpty = self.filteredAttachments?.isEmpty ?? false
+                        self.searchBtn.isHidden = isEmpty
+                        self.noDataImage.isHidden = !isEmpty
+                        self.noDataLabel.isHidden = !isEmpty
+                        self.noDataLabel.text = response.message
+                        self.tv.reloadData()
+                        
+                    } else {
+                        
+                        let isEmpty = self.filteredAttachments?.isEmpty ?? false
+                        
+                        if isEmpty{
+                            self.searchBtn.isHidden = isEmpty
+                            self.noDataImage.isHidden = !isEmpty
+                            self.noDataLabel.isHidden = !isEmpty
+                            self.noDataLabel.text = response.message
+                        }else{
+                            self.ArchiveMessage = response.message ?? ""
+                            self.shouldShowFooterLabel = true
+                        }
+                        self.tv.reloadData()
+                    }
+                    
+                case .failure(let error):
+                    let isEmpty = self.filteredAttachments?.isEmpty ?? false
+                    
+                    if isEmpty{
+                        self.searchBtn.isHidden = isEmpty
+                        self.noDataImage.isHidden = !isEmpty
+                        self.noDataLabel.isHidden = !isEmpty
+                        self.noDataLabel.text = error.localizedDescription
+                    }else{
+                        self.ArchiveMessage = error.localizedDescription
+                        self.shouldShowFooterLabel = true
+                    }
+                    self.tv.reloadData()
+                }
+            }
+        }
+    }
     
     func ReadStatusUpdate(type: String, detail_id: String) {
         APIService.shared.makeApi(
@@ -158,12 +224,12 @@ class AttachmentsVc: UIViewController {
             }
         }
     }
-
+    
 }
 
 extension AttachmentsVc :  UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
     
-  
+    
     
     func scrollToHeaderMessageId(_ messageId: String) {
         // 1. Find section index where header.id == messageId
@@ -197,45 +263,45 @@ extension AttachmentsVc :  UITableViewDataSource,UITableViewDelegate,UISearchBar
         }
     }
     
-//    func tableView(_ tableView: UITableView,
-//                   viewForHeaderInSection section: Int) -> UIView? {
-//        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: CellConfingName.AttachTvHeader) as? AttachTvHeader else { return nil }
-//        
-//        header.configure(with: attachmentHeaders[section])
-//        
-//        header.discretpionLbl
-//            .setupExpandable(
-//                text: attachmentHeaders[section].description ?? "",
-//                isExpanded: attachmentHeaders[section].isExpanded)
-//        
-//        
-//        header.discretpionLbl.onExpandableTap = { [weak self] in
-//            guard let self = self else { return }
-//            
-//            let newValue = !header.discretpionLbl.isExpanded
-//            header.discretpionLbl.isExpanded = newValue
-//            self.attachmentHeaders[section].isExpanded = newValue
-//            
-//                         if self.attachmentHeaders[section].is_unread == true {
-//                             self.ReadStatusUpdate(
-//                                 type: "ATTACHMENT",
-//                                 detail_id: self.attachmentHeaders[section].id ?? "")
-//                         }
-//            
-//            tableView.beginUpdates()
-//            tableView.endUpdates()
-//        }
-//        
-//        header.layoutIfNeeded()
-//        
-//        return header
-//    }
-//    
+    //    func tableView(_ tableView: UITableView,
+    //                   viewForHeaderInSection section: Int) -> UIView? {
+    //        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: CellConfingName.AttachTvHeader) as? AttachTvHeader else { return nil }
+    //
+    //        header.configure(with: attachmentHeaders[section])
+    //
+    //        header.discretpionLbl
+    //            .setupExpandable(
+    //                text: attachmentHeaders[section].description ?? "",
+    //                isExpanded: attachmentHeaders[section].isExpanded)
+    //
+    //
+    //        header.discretpionLbl.onExpandableTap = { [weak self] in
+    //            guard let self = self else { return }
+    //
+    //            let newValue = !header.discretpionLbl.isExpanded
+    //            header.discretpionLbl.isExpanded = newValue
+    //            self.attachmentHeaders[section].isExpanded = newValue
+    //
+    //                         if self.attachmentHeaders[section].is_unread == true {
+    //                             self.ReadStatusUpdate(
+    //                                 type: "ATTACHMENT",
+    //                                 detail_id: self.attachmentHeaders[section].id ?? "")
+    //                         }
+    //
+    //            tableView.beginUpdates()
+    //            tableView.endUpdates()
+    //        }
+    //
+    //        header.layoutIfNeeded()
+    //
+    //        return header
+    //    }
+    //
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredAttachments?.count ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContentCell", for: indexPath) as? ContentCell else {
@@ -259,32 +325,108 @@ extension AttachmentsVc :  UITableViewDataSource,UITableViewDelegate,UISearchBar
                 edit: filteredAttachments?[indexPath.row].can_edit ?? false,
                 delete:  filteredAttachments?[indexPath.row].can_delete ?? false,
                 selectedId: filteredAttachments?[indexPath.row].id ?? ""
-                   )
-//        cell.delegate = self
+            )
+        //        cell.delegate = self
         cell.descriptionLbl
             .setupExpandable(
                 text: filteredAttachments?[indexPath.row].description ?? ""
             )
         cell.descriptionLbl.onExpandableTap = {
-//            cell.descriptionLbl.isExpanded.toggle()
+            //            cell.descriptionLbl.isExpanded.toggle()
             
             let newValue = !cell.descriptionLbl.isExpanded
             cell.descriptionLbl.isExpanded = newValue
             self.filteredAttachments?[indexPath.row].isExpanded = newValue
             
-                         if self.filteredAttachments?[indexPath.row].is_unread == true {
-                             self.ReadStatusUpdate(
-                                 type: "ATTACHMENT",
-                                 detail_id: self.filteredAttachments?[indexPath.row].id ?? "")
-                         }
+            if self.filteredAttachments?[indexPath.row].is_unread == true {
+                self.ReadStatusUpdate(
+                    type: "ATTACHMENT",
+                    detail_id: self.filteredAttachments?[indexPath.row].id ?? "")
+            }
             tableView.beginUpdates()
             tableView.endUpdates()
         }
-//        cell.layoutIfNeeded()
+        //        cell.layoutIfNeeded()
         return cell
     }
     
-
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        
+        let footerView = UIView()
+        footerView.backgroundColor = .clear
+        
+        if shouldShowFooter {
+            // Create a button instead of a label
+            let button = UIButton(type: .system)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.titleLabel?.textAlignment = .right
+            // Create underlined attributed text
+            let title = "See Archived Messages"
+            let attributedTitle = NSAttributedString(
+                string: title,
+                attributes: [
+                    .underlineStyle: NSUnderlineStyle.single.rawValue,
+                    .foregroundColor: UIColor.systemBlue,
+                    .font: UIFont.systemFont(ofSize: 16, weight: .medium)
+                ]
+            )
+            button.setAttributedTitle(attributedTitle, for: .normal)
+            
+            // Add target action
+            button.addTarget(self, action: #selector(seeArchivedMessagesTapped(_:)), for: .touchUpInside)
+            
+            // Add and constrain
+            footerView.addSubview(button)
+            NSLayoutConstraint.activate([
+                button.leadingAnchor.constraint(greaterThanOrEqualTo: footerView.leadingAnchor, constant: 16),
+                button.trailingAnchor.constraint(equalTo: footerView.trailingAnchor, constant: -16),
+                button.topAnchor.constraint(equalTo: footerView.topAnchor, constant: 8),
+                button.bottomAnchor.constraint(equalTo: footerView.bottomAnchor, constant: -8)
+            ])
+            
+        }else if shouldShowFooterLabel {
+            
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = ArchiveMessage
+            label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+            label.textColor = .black
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            
+            footerView.addSubview(label)
+            NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: footerView.leadingAnchor, constant: 16),
+                label.trailingAnchor.constraint(equalTo: footerView.trailingAnchor, constant: -16),
+                label.topAnchor.constraint(equalTo: footerView.topAnchor, constant: 15),
+                label.bottomAnchor.constraint(equalTo: footerView.bottomAnchor, constant: -15),
+                label.centerXAnchor.constraint(equalTo: footerView.centerXAnchor)
+            ])
+        }
+        
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return (shouldShowFooter || shouldShowFooterLabel) ? UITableView.automaticDimension : 0.01
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    @objc private func seeArchivedMessagesTapped(_ sender: UIButton) {
+        print("Archived messages tapped!")
+        
+        fetchAttachments_Archive()
+        
+        shouldShowFooter = false
+    }
+    
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         filterAttachments(with: searchText)
@@ -305,16 +447,16 @@ extension AttachmentsVc :  UITableViewDataSource,UITableViewDelegate,UISearchBar
             self.tv.reloadData()
             self.tv.layoutIfNeeded()
         }
-
+        
         
         if self.filteredAttachments?.isEmpty ?? true {
             self.noDataLabel.isHidden = false
             self.noDataLabel.text = "No Attachment Found"
             
-            self.tv.isHidden = true
+            
         } else {
             self.noDataLabel.isHidden = true
-            self.tv.isHidden = false
+            
         }
     }
 }
--- VsSchoolChimes/SchoolDashBoard/MsgFrmManagement/MsgViewVC.swift
+++ VsSchoolChimes/SchoolDashBoard/MsgFrmManagement/MsgViewVC.swift
@@ -18,20 +18,20 @@ class MsgViewVC: UIViewController,UIScrollViewDelegate {
     @IBOutlet weak var totalduraionLbl: UILabel!
     @IBOutlet weak var AudioFullView: UIView!
     @IBOutlet weak var scrlView: UIScrollView!
-    
     @IBOutlet weak var reasonImgView: UIImageView!
     @IBOutlet weak var reasonDfltLbl: UILabel!
     @IBOutlet weak var voiceTitle: UILabel!
     @IBOutlet weak var reasonLbl: UILabel!
     @IBOutlet weak var titleLbl: UILabel!
-    var player: AVPlayer?
-       var playerItem: AVPlayerItem?
-       var timer: Timer?
-
-       @IBOutlet weak var playButton: UIButton!
-       @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    
     var file_path: [FilePath]?
     var MsgFromManagmentData = ManagemantMessageData()
+    var player: AVPlayer?
+    var playerItem: AVPlayerItem?
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -52,11 +52,18 @@ class MsgViewVC: UIViewController,UIScrollViewDelegate {
         cv.delegate = self
         cv.dataSource = self
        
-        
-        if MsgFromManagmentData.content  != ""{
-            
+        if MsgFromManagmentData.type == "ATTACHMENT"{
+            AudioFullView.isHidden = true
+            cv.isHidden = false
+        }else if MsgFromManagmentData.type == "VOICE"{
+            AudioFullView.isHidden = false
+            cv.isHidden = true
             setupAudio()
+        }else{
+            AudioFullView.isHidden = true
+            cv.isHidden = true
         }
+
         reasonLbl.isHidden = MsgFromManagmentData.description?.isEmpty ?? true
         reasonDfltLbl.isHidden = reasonLbl.isHidden
         reasonImgView.isHidden = reasonLbl.isHidden
