03df24f9aee9f1b7af80f07f6d7ddebda06ed405
murugansavyasasy <murugan@savyasasy.com>
2026-01-24 11:38:13 +0530

    bug fixing by chandhru

--- VsSchoolChimes/SchoolDashBoard/ReuseViews/NotificationCall/NotificationCallVC.swift
+++ VsSchoolChimes/SchoolDashBoard/ReuseViews/NotificationCall/NotificationCallVC.swift
@@ -93,8 +93,6 @@ class NotificationCallVC: UIViewController {
         setupCallerInfo()
         setupSlideToAnswerAnimation()
         addSwipeGesture()
-        speakerBtn.isHidden = true
-        // Initially hide call controls
         cutCallBtn.isHidden = true
         speakerBtn.isHidden = true
         cutCallBtn.layer.cornerRadius = cutCallBtn.frame.width / 2
@@ -759,7 +757,7 @@ class NotificationCallVC: UIViewController {
             self.swipeView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
             
             self.cutCallBtn.isHidden = false
-            self.speakerBtn.isHidden = false
+//            self.speakerBtn.isHidden = false
             
             self.cutCallBtn.alpha = 1.0
             self.durationLbl.alpha = 1.0
