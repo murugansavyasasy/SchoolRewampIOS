61234c04d01c6bcc8be4770c070110823e177bc6
murugansavyasasy <murugan@savyasasy.com>
2026-06-13 14:04:26 +0530

    message content chnages by saran

--- Features/CommonScreens/SchoolList/SchoolListVC.swift
+++ Features/CommonScreens/SchoolList/SchoolListVC.swift
@@ -688,8 +688,8 @@ class SchoolListVC: UIViewController,UITableViewDelegate,UITableViewDataSource{
         APIService.shared
             .makeApi(url: ServiceUrl.comm_text_message_send_text, parameters:[
                 
-                send_textmessageStringFile.description : user_inputs.title,
-                send_textmessageStringFile.message : user_inputs.description,
+                send_textmessageStringFile.description :  user_inputs.description,
+                send_textmessageStringFile.message :user_inputs.title,
                 send_textmessageStringFile.target_code: array_selectedSchoolId,
                 send_textmessageStringFile.target_type: TargetTypes.school,
                 send_textmessageStringFile.academic_year_id: selectedAcadimicYearId ?? 0
